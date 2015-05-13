f238566ff713680850353c8ca37e8773c297620f
Stoemp Saucisse <stoempsaucisse@hotmail.com>
2015-05-13 16:18:44 +0200

    Edit config file

---
+++ src/config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Database settings
+    |--------------------------------------------------------------------------
+    |
+    | The name of the table to create in the database
+    |
+    */
+    'table_name' => env('MO_UNITS_TABLE_NAME', 'units'),
+
+);
